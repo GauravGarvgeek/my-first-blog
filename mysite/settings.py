65860b714723c1125aff0e4a64c31f234b7a179a
Gaurav <gaurav.official.001@gmail.com>
2020-01-27 17:56:32 +0530

    bug fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x#n+azxn=@!(h3%4p+d#d^)mzfc(2pa@)soi4jm&m7!v&@rs&q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com','gauravgarv.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
 
 
 # Application definition
